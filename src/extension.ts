c3b6e10acc15344abde845881627c1e1c00e37d4
William Getz <willegetz@gmail.com>
2020-06-17 10:48:08 -0700

    Removed comments about error

--- src/extension.ts
+++ src/extension.ts
@@ -6,9 +6,6 @@ export function activate(context: vscode.ExtensionContext) {
 	const sample = container.build('sample');
 	sample.run();
 
-	// Activating extension 'spike.helloworld' failed: Unable to parse arguments from function or expression: Sample.
-	// Command 'Hello World' resulted in an error (command 'helloworld.helloWorld' not found)
-
 	console.log('Congratulations, your extension "helloworld" is now active!');
 
 	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
