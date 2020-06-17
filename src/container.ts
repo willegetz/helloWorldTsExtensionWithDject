584f332276cb7d92c8b0535cf9bc1f0301d8e921
William Getz <willegetz@gmail.com>
2020-06-16 17:35:51 -0700

    start building with dject

---
+++ src/container.ts
@@ -0,0 +1,8 @@
+
+
+const dject = require('dject');
+const container = dject.new({});
+
+
+
+export default container;
---
+++ src/modules/Sample.ts
@@ -0,0 +1,12 @@
+class Sample {
+    constructor() {
+
+    }
+}
+
+Sample['@instantiable'] = true;
+
+export default {
+    name: 'sample',
+    value: Sample
+}
