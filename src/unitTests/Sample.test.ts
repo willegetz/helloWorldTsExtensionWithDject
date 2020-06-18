61197a057e464e654fb8a1ed21de25c2efed1fef
William Getz <willegetz@gmail.com>
2020-06-18 16:07:39 -0700

    Cleaned up test

--- src/unitTests/Sample.test.ts
+++ src/unitTests/Sample.test.ts
@@ -1,16 +1,23 @@
 import container from '../container';
-import * as vscode from 'vscode';
 import { describe, it, beforeEach } from 'mocha';
 import * as sinon from 'sinon';
 import { assert } from 'chai';
 
 describe('Sample class', function () {
+    const testContainer = container.new();
+    console.log('Container: Registerd Modules: ', JSON.stringify(container.getRegisteredModules(), null, 4));
+    console.log('TestContainer: Registerd Modules: ', JSON.stringify(testContainer.getRegisteredModules(), null, 4));
+
+
     let sample;
     let runSpy;
+    let vscode;
 
     beforeEach(function () {
         sample = container.build('sample');
         runSpy = sinon.spy(sample, "run");
+        
+        vscode = testContainer.build('vscodeFactory');
     })
 
     describe('Run', () => {
