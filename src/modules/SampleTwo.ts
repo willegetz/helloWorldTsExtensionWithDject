af217a4152c44478733f9d05c61dd4fb0fe5f5ca
William Getz <willegetz@gmail.com>
2020-06-18 14:38:36 -0700

    Can load module from vscode via dject using the typescript 'client' dject module format

--- src/modules/SampleTwo.ts
+++ src/modules/SampleTwo.ts
@@ -1,17 +1,31 @@
+import vscodeFactory from "./vscodeFactory";
+
 const dependencies = [
-    '__container'
+    '__container',
+    'vscodeFactory'
 ]
 
 class SampleTwo {
+    private vscodeFactory;
+
     constructor(...injectedDependencies){
         const [container] = injectedDependencies;
-        const {} = container.buildDependencyMap(dependencies, injectedDependencies);
+        const {
+            vscodeFactory
+        } = container.buildDependencyMap(dependencies, injectedDependencies);
+
+        this.vscodeFactory = vscodeFactory;
 
         console.log('In the constructor of Sample Two');
     }
 
     logOutData(){
         console.log('Sample Two is logging out data');
+
+        const uri = this.vscodeFactory.getUri()
+        const uriPath = uri.parse('d:/another/path/exists.txt');
+
+        console.log('SampleTwo logs out the following URI: ', uriPath)
     }
 }
 
