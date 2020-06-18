fb6c1f031a77d99cd18aa522115d17aea8871c05
William Getz <willegetz@gmail.com>
2020-06-18 14:58:41 -0700

    Removed abandoned dynamnic import attempt

--- src/modules/vscodeFactory.ts
+++ src/modules/vscodeFactory.ts
@@ -9,28 +9,7 @@ class VscodeFactory {
         this.localVscode = vscode.Uri;
     }
 
-    private vscodeProvider(callback){
-        import('vscode').then(module => {
-            console.log('We have the vscode');
-            callback(module)
-        });
-    }
-
-    private async getVsCode(){
-        this.localVscode = await import('vscode');
-    }
-
-    async getAsync(){
-        await this.getVsCode()
-        return this.localVscode;
-    }
-
     getUri() {
-        // const callback = function(vscodeModule){
-        //     this.localVsode = vscodeModule;
-        // }
-
-        // this.vscodeProvider(callback);
         console.log('Trying to get vscode.Uri');
         return this.localVscode;
 
