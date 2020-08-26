96d14933f71ccf9d235ac87b92a3e7eb373bf3a3
William Getz <willegetz@gmail.com>
2020-08-26 13:09:57 -0700

    Replacing hard coded windows path separator in es-module-config, dject no longer finds the otherSamples folder

--- src/container.ts
+++ src/container.ts
@@ -1,7 +1,6 @@
-import module0 from '../src/mixedModules/otherSamples/SampleThree';
-import module1 from '../src/modules/Sample';
-import module2 from '../src/modules/SampleTwo';
-import module3 from '../src/modules/vscodeFactory';
+import module0 from '../src/modules/Sample';
+import module1 from '../src/modules/SampleTwo';
+import module2 from '../src/modules/vscodeFactory';
 
 
 const dject = require('dject');
@@ -16,9 +15,6 @@ container.register(module1.value, module1.name);
 
 container.copyProps(module2.value, module2);
 container.register(module2.value, module2.name);
-
-container.copyProps(module3.value, module3);
-container.register(module3.value, module3.name);
 
 
 export default container;
